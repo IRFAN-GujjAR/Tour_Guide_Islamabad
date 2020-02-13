671395284489584e304980a95062f7f3ef23f0ee
IRFAN-GujjAR <irfangujjar1684@gmail.com>
2020-02-13 19:17:36 +0500

    Grade version updated

--- app/src/main/java/com/example/irfan/tourguide/MainActivity.java
+++ app/src/main/java/com/example/irfan/tourguide/MainActivity.java
@@ -1,17 +1,14 @@
 package com.example.irfan.tourguide;
 
-import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         ActionBar actionBar=getSupportActionBar();
+        assert actionBar != null;
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
 
